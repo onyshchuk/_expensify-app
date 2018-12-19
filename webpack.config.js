06066980a897ae124f4bb56613c8e9d5103f35aa
Onuk <onishchuk.alexander@gmail.com>
2018-12-19 12:52:05 +0200

    Setup production webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,43 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-   entry: './src/app.js',
-   output: {
-      path: path.join(__dirname, 'public'),
-      filename: 'bundle.js'
-   },
-   module: {
-      rules: [{
-         loader: 'babel-loader',
-         test: /\.js$/,
-         exclude: /node_modules/,
-      }, {
-         test: /\.s?css$/,
-         use: [
-            'style-loader',
-            'css-loader',
-            'sass-loader'
-         ]
-      }]
-   },
-   devtool: 'cheap-module-eval-source-map',
-   devServer: {
-      contentBase: path.join(__dirname, 'public'),
-      historyApiFallback: true
-   }
+module.exports = (env) => {
+   const isProduction = env === 'production';
+
+   return {
+      entry: './src/app.js',
+      output: {
+         path: path.join(__dirname, 'public'),
+         filename: 'bundle.js'
+      },
+      module: {
+         rules: [{
+            loader: 'babel-loader',
+            test: /\.js$/,
+            exclude: /node_modules/,
+         }, {
+            test: /\.s?css$/,
+            use: [
+               // isProduction ? MiniCssExtractPlugin.loader : 'style-loader', // good for js
+               MiniCssExtractPlugin.loader,
+               {
+                  loader: 'css-loader',
+                  options: { sourceMap: true }
+               },
+               {
+                  loader: 'sass-loader',
+                  options: { sourceMap: true }
+               }
+            ]
+         }]
+      },
+      plugins: [
+         new MiniCssExtractPlugin({ filename: 'styles.css' })
+      ],
+      devtool: isProduction ? 'source-map' : 'inline-source-map', // cheap-module-eval-source-map // goof for js
+      devServer: {
+         contentBase: path.join(__dirname, 'public'),
+         historyApiFallback: true
+      }
+   };
 };
